f8850110428538e271459dc92d0d68586e82588a
John Leach <john@johnleach.co.uk>
2009-03-02 22:35:19 +0000

    Some rdoc. Change << method to :add_doc

--- lib/xapian_fu.rb
+++ lib/xapian_fu.rb
@@ -11,20 +11,31 @@ class XapianDb
     @db_flag = DB_CREATE_OR_OVERWRITE if options[:overwrite]
   end
 
+  # Return the writable Xapian database
   def db
     @db ||= WritableDatabase.new(dir, db_flag)
   end
 
-  def <<(doc)
+  # Add a document to the index. A document is just a hash, the keys representing
+  # field names and their values the data to be indexed.
+  # 
+  # Any value stored with the special key :data is marhalled and stored in the 
+  # Xapian database.  Any arbitrary data up to Xmeg can be stored here.  This is
+  # often used to store a reference to another data storage system, such as the
+  # primary key of an SQL database table.
+  def add_doc(doc)
     xdoc = Document.new
     xdoc.data = Marshal.dump(doc.data) if doc.data
     tg = TermGenerator.new
     tg.database = db
     tg.document = xdoc
-    tg.index_text( doc.fields.keys.collect{|k|doc.fields[k]}.join(' ') )
+    tg.index_text( doc.fields.keys.collect { |k| doc.fields[k] }.join(' ') )
     db.add_document(xdoc)
   end
+  alias_method "<<", :add_doc
 
+  # Conduct a search on the Xapian database, returning an array of 
+  # XapianResult objects
   def search(q, options = {})
     defaults = { :offset => 0, :limit => 10 }
     options = defaults.merge(options)
@@ -33,6 +44,12 @@ class XapianDb
     enquiry.mset(options[:offset], options[:limit]).matches.collect { |m| XapianResult.new(m) }
   end
 
+  # Run the given block in a XapianDB transaction.  Any changes to the 
+  # Xapian database made in the block will be atomically committed at the end.
+  # 
+  # If an exception is raised by the block, all changes are discarded and the
+  # exception re-raised.
+  #
   def transaction
     db.begin_transaction
     yield
@@ -42,6 +59,7 @@ class XapianDb
     raise e
   end
 
+  # Flush any changes to disk.
   def flush
     db.flush
   end
@@ -68,6 +86,8 @@ class XapianDoc
   end
 end
 
+# A XapianResult objects represents a document in the Xapian DB that matched
+# a query.
 class XapianResult
   attr_reader :score, :percent, :doc, :data, :id
   def initialize(match)
